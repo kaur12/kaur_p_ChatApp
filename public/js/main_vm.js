d3d9b4cc1c0bc7fa0112336f2419c0091b60ac57
Kaur <p_kaur92032@ats-hr420-dm11.fc.ca>
2020-01-28 17:30:24 -0500

    latest

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,16 +1,50 @@
 // imports always go first - if we're importing anything
+import ChatMessage from "./modules/ChatMessage.js";
 
 const socket = io();
 
-function setUserId(packet) {
-    debugger;
-    console.log(packet);
+function setUserId(sID, message) {
+    //debugger;
+    vm.socketID = sID;
 }
 
 function runDisconnectMessage(packet) {
-    debugger;
+    //debugger;
     console.log(packet);
 }
+
+// this is our main Vue instance
+const vm = new Vue({
+    data: {
+        socketID: "",
+        messages: [],
+        message: "",
+        nickName: ""
+    },
+
+    methods: {
+        dispatchMessage() {
+            // emit a message event and send the message to the server
+            console.log('handle send message');
+
+            socket.emit('chat_message', {
+                content: this.message,
+                name: this.nickName || "anonymous"
+                // || is called double pipe operator or an "or" operator
+                // is this.nickName is set, use it as the value
+                // or just make name "anonymous"
+            })
+        }
+    },
+
+    components: {
+        newmessage: ChatMessage
+    },
+
+    mounted: function() {
+        console.log('mounted');
+    }
+}).$mount("#app");
 
 // some event handling -> these events are coming from the server
 socket.addEventListener('connected', setUserId);
